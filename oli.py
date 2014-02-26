52502b401f50f6fd2a3ae6b7055649f0172ee100
Zacharias Knudsen <zachasme@gmail.com>
2014-02-26 21:08:36 +0100

    Set unknowns to mean

--- oli.py
+++ oli.py
@@ -7,18 +7,46 @@ attributeNames = [
 'communityname','State','countyCode','communityCode','fold','pop','perHoush','pctBlack','pctWhite','pctAsian','pctHisp','pct12-21','pct12-29','pct16-24','pct65up','persUrban','pctUrban','medIncome','pctWwage','pctWfarm','pctWdiv','pctWsocsec','pctPubAsst','pctRetire','medFamIncome','perCapInc','whitePerCap','blackPerCap','NAperCap','asianPerCap','otherPerCap','hispPerCap','persPoverty','pctPoverty','pctLowEdu','pctNotHSgrad','pctCollGrad','pctUnemploy','pctEmploy','pctEmployMfg','pctEmployProfServ','pctOccupManu','pctOccupMgmt','pctMaleDivorc','pctMaleNevMar','pctFemDivorc','pctAllDivorc','persPerFam','pct2Par','pctKids2Par','pctKids-4w2Par','pct12-17w2Par','pctWorkMom-6','pctWorkMom-18','kidsBornNevrMarr','pctKidsBornNevrMarr','numForeignBorn','pctFgnImmig-3','pctFgnImmig-5','pctFgnImmig-8','pctFgnImmig-10','pctImmig-3','pctImmig-5','pctImmig-8','pctImmig-10','pctSpeakOnlyEng','pctNotSpeakEng','pctLargHousFam','pctLargHous','persPerOccupHous','persPerOwnOccup','persPerRenterOccup','pctPersOwnOccup','pctPopDenseHous','pctSmallHousUnits','medNumBedrm','houseVacant','pctHousOccup','pctHousOwnerOccup','pctVacantBoarded','pctVacant6up','medYrHousBuilt','pctHousWOphone','pctHousWOplumb','ownHousLowQ','ownHousMed','ownHousUperQ','ownHousQrange','rentLowQ','rentMed','rentUpperQ','rentQrange','medGrossRent','medRentpctHousInc','medOwnCostpct','medOwnCostPctWO','persEmergShelt','persHomeless','pctForeignBorn','pctBornStateResid','pctSameHouse-5','pctSameCounty-5','pctSameState-5','numPolice','policePerPop','policeField','policeFieldPerPop','policeCalls','policCallPerPop','policCallPerOffic','policePerPop2','racialMatch','pctPolicWhite','pctPolicBlack','pctPolicHisp','pctPolicAsian','pctPolicMinority','officDrugUnits','numDiffDrugsSeiz','policAveOT','landArea','popDensity','pctUsePubTrans','policCarsAvail','policOperBudget','pctPolicPatrol','gangUnit','pctOfficDrugUnit','policBudgetPerPop','murders','murdPerPop','rapes','rapesPerPop','robberies','robbbPerPop','assaults','assaultPerPop','burglaries','burglPerPop','larcenies','larcPerPop','autoTheft','autoTheftPerPop','arsons','arsonsPerPop','violentPerPop','nonViolPerPop',
 ]
 
+discard_columns_up_to = 5;
+
 csvfile = open('data/communities.data')
 csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-data = list(csvreader)
-M = len(attributeNames)-5
-N = len(data)
+
+fullrows = list(csvreader)
+rows = []
+for fullrow in fullrows:
+    rows.append(fullrow[discard_columns_up_to:])
+
+
+M = len(rows[1])
+N = len(rows)
 
 X = np.zeros(shape=(N,M))
 
-for idx, row in enumerate(data):
-    while '?' in row:
-        row[row.index('?')] = -1
-    X[idx,:] = row[5:]
+# find columns with unknowns
+unknown_columns = set();
+for i,row in enumerate(rows):
+    for j,cell in enumerate(row):
+        if cell == '?':
+            unknown_columns.add(j)
+        else:
+            rows[i][j] = float(cell)
+
+means = {}
+for c in unknown_columns:
+    summed = 0;
+    n = 0;
+    for r in rows:
+        if r[c] != '?':
+            summed += r[c]
+            n += 1
+    means[c] = summed / n
+
+for i,row in enumerate(rows):
+    for j,cell in enumerate(row):
+        if cell == '?':
+            row[j] = means[j] # mean?
+    X[i,:] = row
 
 ## P C fucking A
 #subtract mean
@@ -43,7 +71,7 @@ j = 1
 
 # Plot PCA of the data
 figure()
-title('NanoNose data: PCA')
+title('PCAAAAAAAAAAAAA')
 plot(Z[:,i], Z[:,j], 'o')
 
 # Output result to screen
