d487cc11e9cd61705f9ecc0bd1744b4774d65c6f
henrik <henrik@henrik-XPS1.(none)>
2014-03-04 00:50:33 +0100

    tex

--- olioli.py
+++ olioli.py
@@ -3,5 +3,5 @@ import matplotlib.pyplot as plt
 from pandas.tools.plotting import parallel_coordinates
 
 data = pandas.read_csv(r'C:\Python27\Lib\site-packages\pandas\tests\data\iris.csv', sep=',')
-pandasd.parallel_coordinates(data, 'Name')
+parallel_coordinates(data, 'Name')
 plt.show()
