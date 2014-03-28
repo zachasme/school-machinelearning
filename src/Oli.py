63f948eb442a5d2900e7497bb339b688f2b858a4
zachasme <zachasme@gmail.com>
2014-03-28 15:50:18 +0100

    Push it motherucker

--- src/Oli.py
+++ src/Oli.py
@@ -1,11 +1,19 @@
-#from pylab import *
+from enum import Enum
 
 import pandas as pd
 import numpy as np
+
+#from pylab import *
 #import matplotlib.pyplot as plt
 
 
-class Oli:
+Fixna = Enum('Fixna', 'FILLMEAN DROPOBJECTS DROPATTRIBUTES')
+
+
+
+class DataSet:
+
+
 	# X: data matrix, rows correspond to N data objects, each of which contains M attributes
 	# N: number of data objects
 	# M: number of attributes
@@ -16,20 +24,55 @@ class Oli:
 # classNames:     a Cx1 matrix
 # C:              number of classes
 
-	def __init__(self, data):
-		self.X = pd.DataFrame(data)
-		self.__normalize()
-		#self.__standardize()
+	def __init__(self, data, names, **options):
+		self.options = {
+			'fixna':     options.get('fixna', None),
+			'standardize': options.get('normalize', False),
+			'normalize': options.get('normalize', False),
+		}
 
-	def __normalize(self):
-		"""Adjust all values to be between 0 and 1"""
-		max_values = self.X.max(axis=1)
-		self.X = max_values
-		#self.X = self.X / max_values[:, np.newaxis]
+		"""Create the data set"""
+		self.X = pd.DataFrame(data, columns=names)
+
+		"""Fix missing values"""
+		if self.options['fixna'] != None:
+			self.__fixna()
+
+		#self.__normalize()
+		self.__standardize()
+
+	def __str__(self):
+		return str(self.X)
+
+
+
+	def __fixna(self):
+		"""Fixes missing values. How depends on option"""
+		if not isinstance( self.options['fixna'], Fixna ):
+			raise Exception("Option 'fixna' was given value " + str(self.options['fixna']) + ", but needs to be one of " + str(list(Fixna)))
+
+		if self.options['fixna'] == Fixna.FILLMEAN:
+			self.X.fillna(self.X.mean())
+		if self.options['fixna'] == Fixna.DROPOBJECTS:
+			self.X = self.X.dropna(axis=0);
+		if self.options['fixna'] == Fixna.DROPATTRIBUTES:
+			self.X = self.X.dropna(axis=1);
+
+
 
 	def __standardize(self):
 		"""Make sure variance is 1"""
-		self.X = (self.X - self.X_mean) / std(X)
+		self.X = (self.X - self.X.mean()) / self.X.std();
+
+
+
+	def __normalize(self):
+		"""Adjust all values to be between 0 and 1"""
+		max_values = self.X.max(axis=0)
+		self.X = max_values[:,np.newaxis]
+		#self.X = self.X / max_values[:, np.newaxis]
+
+
 
 	def mean(self):
 		"""Calculates means along TODO:WHAT?"""
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import csv
+import numpy as np
 
+PREDISCARD = 5 # discard first DISCARD columns
 DISCARD = 5 # discard first DISCARD columns
 DATADIR = '../data/'
 
@@ -11,17 +13,18 @@ datafile = open(DATADIR + 'communities.data')
 datareader = csv.reader(datafile, delimiter=',', quotechar='|')
 namereader = csv.reader(namefile, delimiter=',', quotechar='|')
 
-data = [[ '?' if cell is '?' else float(cell)
+data = [[ np.nan if cell is '?' else float(cell)
           for cell in row[DISCARD:]] for row in datareader]
+names = next(namereader)
 
 
 
 
+from Oli import *
 
-import Oli
+oli = DataSet(data, names[DISCARD:], fixna=Fixna.FILLMEAN, standardize=True)
 
-oli = Oli.Oli(data)
-
-print(oli.X)
+print(oli.X.std())
+print(oli.X.mean())
 
 #Oli.PCA(oli)
