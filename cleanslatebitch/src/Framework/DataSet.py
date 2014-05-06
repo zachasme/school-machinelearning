6361fb3faf63538ba8994f82638a2a25371d876b
zachasme <zachasme@gmail.com>
2014-05-06 11:16:45 +0200

    OH LORD BLACK DOLPHINS

--- cleanslatebitch/src/Framework/DataSet.py
+++ cleanslatebitch/src/Framework/DataSet.py
@@ -59,7 +59,7 @@ class DataSet:
 		if self._classes is None:
 			raise Exception("DataSet: reading y property, but class-column not set")
 
-		return self._classes.apply(lambda x: np.nan if x is np.nan else self.classNames.tolist().index(x)).as_matrix()
+		return self._classes.apply(lambda x: np.nan if x is np.nan else self.classNames.tolist().index(x)).values
 
 
 
@@ -68,7 +68,7 @@ class DataSet:
 
 
 
-	def __init__( self, datafile=None, na_values=[], dataframe=None, string_columns=[], class_column=None, classes=None ):
+	def __init__( self, datafile=None, na_values=[], dataframe=None, string_columns=[], classes=None ):
 		"""Creates the data set"""
 
 		# if filepath is given, read as csv
@@ -79,9 +79,6 @@ class DataSet:
 			self.df = dataframe.copy()
 
 		self._classes = classes
-		if class_column is not None:
-			self.df = self.set_class_column(class_column).df
-
 
 		# convert string columns to indices
 		for c in string_columns:
--- cleanslatebitch/src/Framework/PCA.py
+++ cleanslatebitch/src/Framework/PCA.py
@@ -17,37 +17,40 @@ class PCA:
 	def __init__(self, dataset):
 		"""Performs the principal component analysis"""
 		# First, nominals and rows with missing values must be dropped
-		dataset = dataset.drop_nominals().fix_missing(drop_objects=True)
+		dataset = dataset.fix_missing(drop_objects=True)
+		self.df = dataset.df;
 
 		# Center to zero mean
-		Y = dataset.center().X
+		Y = dataset.standardize().X
 
 		U,S,V = np.linalg.svd(Y, full_matrices=False)
-		print(S)
+
 		# computes variance explained by principal components
 		self.rho = pd.Series((S*S) / (S*S).sum())
 		# projects the centered data onto principal component space, Z
 		V = V.T
 		self.Z = Y.dot(V)
 
-	def plot(self):
-		pl.subplot(2, 2, 1)
-		pl.scatter(self.Z[:, 0], self.Z[:, 1])
+	def plot(self, color=None):
+		pl.figure(1)
+		pl.scatter(self.Z[:, 0], self.Z[:, 1], c=self.df[color], cmap=pl.get_cmap('Oranges'))
+		print(self.df[color])
 		pl.title('Transformed data')
 		pl.xlabel('PCA #1')
 		pl.ylabel('PCA #2')
 
-		pl.subplot(2, 2, 2)
-#		parallel_coordinates(self.dataset.df, 'communityCode')
 
-		pl.subplot(2, 2, 3)
+		pl.figure(2)
 		cumsum = self.rho.cumsum()
 		cumsum.plot()
 		pl.title('Varaince')
 		pl.xlabel('Number of principal components')
-		pl.ylabel('Explained Varaince')
+		pl.ylabel('Explained Variance')
 
 		pl.show()
+
+
+
 
 	def auto(self):
 		pca = decomposition.PCA(n_components=2)
