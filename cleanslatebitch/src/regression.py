1d540c4bc17e4d1d68e31de37e64c47bd00aaa75
Valentin <valentin.rosenberg@gmail.com>
2014-05-06 12:08:49 +0200

    lol

--- cleanslatebitch/src/regression.py
+++ cleanslatebitch/src/regression.py
@@ -1,8 +1,13 @@
 import csv
 import numpy as np
+from Framework.DataSet import *
+
+from pylab import *
+from scipy.io import loadmat
 
 DATADIR = '../data/'
 
+crime = DataSet(datafile='../data/normalized.csv')
 
 drop_columns = [
 	'State', 'countyCode', 'communityCode', 'communityname',
@@ -20,34 +25,24 @@ drop_columns = [
 ]
 
 
-from DataSet import *
-from PCA import *
+crime = DataSet(datafile='../data/raw.csv', na_values=["?"], string_columns=['state', 'communityname'])
 
-#print("\n\nstd:",   oli.X.std())
-#print("\n\nmean:",  oli.X.mean())
-#print("\n\nrange:", oli.X.max()-oli.X.min())
-
-#Oli.PCA(oli)
-
-crime = DataSet(datafile='../data/normalized.csv')
-
-crime = crime.drop(['state', 'communityname'])       # Drop strings
-crime = crime.drop(['countyCode','communityCode']) # Drop nominals
 crime = crime.drop_columns([
   'fold',
   'murders', 'murdPerPop',
   'rapes', 'rapesPerPop',
   'robberies', 'robbbPerPop',
-# 'assaults', 'assaultPerPop',
+  'assaults', 'assaultPerPop',
   'burglaries', 'burglPerPop',
   'larcenies', 'larcPerPop',
-  'autoTheft', 'autoTheftPerPop',
+  #'autoTheft', 'autoTheftPerPop',
   'arsons', 'arsonsPerPop',
   'ViolentCrimesPerPop',
-  'nonViolPerPop',
+  'nonViolPerPop'
 ])
 
-crime = DataSet(dataframe=crime.df[:200])
+crime = crime.take_first_n_rows(200)
+crime = crime.fix_missing(fill_mean=True)
 #regression
 from pylab import *
 from sklearn import cross_validation
@@ -59,7 +54,6 @@ import sklearn.linear_model as lm
 attributeNames = crime.attributeNames
 
 import pandas as pd
-import neurolab as nl
 import scipy.linalg as linalg
 from pylab import *
 from scipy import stats
@@ -70,26 +64,26 @@ crime = crime.drop_columns('autoTheftPerPop')
 #X = X[50:70,:]
 X = crime.X
 
-print(y)
-'''
-# Fit ordinary least squares regression model
-model = lm.LinearRegression()
-model.fit(X,y)
+#print(y)
 
-# Predict autotheftperpop
-y_est = model.predict(X)
-residual = y_est - y
+# # Fit ordinary least squares regression model
+# model = lm.LinearRegression()
+# model.fit(X,y)
 
-# Display scatter plot
-figure()
-subplot(2, 1, 1)
-plot(y, y_est, '.')
-xlabel('autoTheftPerPop (true)'); ylabel('autoTheftPerPop (estimated)');
-subplot(2, 1, 2)
-hist(residual, 40)
+# # Predict autotheftperpop
+# y_est = model.predict(X)
+# residual = y_est - y
 
-show()
-'''
+# # Display scatter plot
+# figure()
+# subplot(2, 1, 1)
+# plot(y, y_est, '.')
+# xlabel('autoTheftPerPop (true)'); ylabel('autoTheftPerPop (estimated)');
+# subplot(2, 1, 2)
+# hist(residual, 40)
+
+# show()
+
 
 N, M = X.shape
 
@@ -197,42 +191,42 @@ for train_index, test_index in CV:
 
     k+=1
 
-'''
-X = X[:,selected_features]
+# '''
+# X = X[:,selected_features]
 
-N, M = X.shape
+# N, M = X.shape
 
-## Crossvalidation
-# Create crossvalidation partition for evaluation
-K = 3
-CV = cross_validation.KFold(N,K,shuffle=True)
-k=0
-for train_index, test_index in CV:
-    print('\nCrossvalidation fold: {0}/{1}'.format(k+1,K))
+# ## Crossvalidation
+# # Create crossvalidation partition for evaluation
+# K = 3
+# CV = cross_validation.KFold(N,K,shuffle=True)
+# k=0
+# for train_index, test_index in CV:
+#     print('\nCrossvalidation fold: {0}/{1}'.format(k+1,K))
 
-    # extract training and test set for current CV fold
-    X_train = X[train_index,:]
-    y_train = y[train_index,:]
-    X_test = X[test_index,:]
-    y_test = y[test_index,:]
+#     # extract training and test set for current CV fold
+#     X_train = X[train_index,:]
+#     y_train = y[train_index,:]
+#     X_test = X[test_index,:]
+#     y_test = y[test_index,:]
 
-    best_train_error = 1e100
-    for i in range(n_train):
-        print('Training network {0}/{1}...'.format(i+1,n_train))
-        # Create randomly initialized network with 2 layers
-        ann = nl.net.newff([[0, 1]]*M, [n_hidden_units, 1], [nl.trans.TanSig(),nl.trans.PureLin()])
-        # train network
-        train_error = ann.train(X_train, y_train, goal=learning_goal, epochs=max_epochs)
-        if train_error[-1]<best_train_error:
-            bestnet[k]=ann
-            best_train_error = train_error[-1]
-            error_hist[range(len(train_error)),k] = train_error
+#     best_train_error = 1e100
+#     for i in range(n_train):
+#         print('Training network {0}/{1}...'.format(i+1,n_train))
+#         # Create randomly initialized network with 2 layers
+#         ann = nl.net.newff([[0, 1]]*M, [n_hidden_units, 1], [nl.trans.TanSig(),nl.trans.PureLin()])
+#         # train network
+#         train_error = ann.train(X_train, y_train, goal=learning_goal, epochs=max_epochs)
+#         if train_error[-1]<best_train_error:
+#             bestnet[k]=ann
+#             best_train_error = train_error[-1]
+#             error_hist[range(len(train_error)),k] = train_error
 
-    print('Best train error: {0}...'.format(best_train_error))
-    y_est = bestnet[k].sim(X_test)
-    errors[k] = np.power(y_est-y_test,2).sum().astype(float)/y_test.shape[0]
-    k+=1
-'''
+#     print('Best train error: {0}...'.format(best_train_error))
+#     y_est = bestnet[k].sim(X_test)
+#     errors[k] = np.power(y_est-y_test,2).sum().astype(float)/y_test.shape[0]
+#     k+=1
+# '''
 
 print('\n')
 print('Linear regression without feature selection:\n')
@@ -246,15 +240,15 @@ print('- Test error:     {0}'.format(Error_test_fs.mean()))
 print('- R^2 train:     {0}'.format((Error_train_nofeatures.sum()-Error_train_fs.sum())/Error_train_nofeatures.sum()))
 print('- R^2 test:     {0}'.format((Error_test_nofeatures.sum()-Error_test_fs.sum())/Error_test_nofeatures.sum()))
 
-[tstatistic, pvalue] = stats.ttest_ind(Error_train,Error_train_fs)
-if pvalue<=0.05:
-    print('Regressions are significantly different. (p={0})'.format(pvalue[0]))
-else:
-    print('Regressions are not significantly different (p={0})'.format(pvalue[0]))
-figure(20)
-boxplot(np.bmat('Error_train, Error_train_fs'))
-xlabel('Regression without feature selection  vs.  Regression with feature selection')
-ylabel('Cross-validation error [%]')
+# [tstatistic, pvalue] = stats.ttest_ind(Error_train,Error_train_fs)
+# if pvalue<=0.05:
+#     print('Regressions are significantly different. (p={0})'.format(pvalue[0]))
+# else:
+#     print('Regressions are not significantly different (p={0})'.format(pvalue[0]))
+# figure(20)
+# boxplot(np.bmat('Error_train, Error_train_fs'))
+# xlabel('Regression without feature selection  vs.  Regression with feature selection')
+# ylabel('Cross-validation error [%]')
 
 figure(k)
 subplot(1,3,2)
@@ -264,31 +258,31 @@ xlabel('Crossvalidation fold')
 ylabel('Attribute')
 
 
-# Inspect selected feature coefficients effect on the entire dataset and
-# plot the fitted model residual error as function of each attribute to
-# inspect for systematic structure in the residual
-f=2 # cross-validation fold to inspect
-ff=Features[:,f-1].nonzero()[0]
-m = lm.LinearRegression().fit(X[:,ff], y)
+# # Inspect selected feature coefficients effect on the entire dataset and
+# # plot the fitted model residual error as function of each attribute to
+# # inspect for systematic structure in the residual
+# f=2 # cross-validation fold to inspect
+# ff=Features[:,f-1].nonzero()[0]
+# m = lm.LinearRegression().fit(X[:,ff], y)
 
-y_est= m.predict(X[:,ff])
-residual=y-y_est
+# y_est= m.predict(X[:,ff])
+# residual=y-y_est
 
-figure(k+1)
-title('Residual error vs. Attributes for features selected in cross-validation fold {0}'.format(f))
-for i in range(0,len(ff)):
-   subplot(2,ceil(len(ff)/2.0),i+1)
-   plot(X[:,ff[i]],residual,'.')
-   xlabel(attributeNames[ff[i]])
-   ylabel('residual error')
+# figure(k+1)
+# title('Residual error vs. Attributes for features selected in cross-validation fold {0}'.format(f))
+# for i in range(0,len(ff)):
+#    subplot(2,ceil(len(ff)/2.0),i+1)
+#    plot(X[:,ff[i]],residual,'.')
+#    xlabel(attributeNames[ff[i]])
+#    ylabel('residual error')
 
-'''
-print('Mean-square error: {0}'.format(mean(errors)))
-figure(k+2);
-subplot(2,1,1); bar(range(0,K),errors); title('Mean-square errors');
-subplot(2,1,2); plot(error_hist); title('Training error as function of BP iterations');
-figure(k+3);
-subplot(2,1,1); plot(y_est); plot(y_test); title('Last CV-fold: est_y vs. test_y'); 
-subplot(2,1,2); plot(y_est-y_test); title('Last CV-fold: prediction error (est_y-test_y)');
-'''
-show()  
+# '''
+# print('Mean-square error: {0}'.format(mean(errors)))
+# figure(k+2);
+# subplot(2,1,1); bar(range(0,K),errors); title('Mean-square errors');
+# subplot(2,1,2); plot(error_hist); title('Training error as function of BP iterations');
+# figure(k+3);
+# subplot(2,1,1); plot(y_est); plot(y_test); title('Last CV-fold: est_y vs. test_y'); 
+# subplot(2,1,2); plot(y_est-y_test); title('Last CV-fold: prediction error (est_y-test_y)');
+# '''
+# show()  
